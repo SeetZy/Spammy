7ce4d39abd6a1ee5c1b05e4e77d22dffd159be7a
SeetZy <64326465+SeetZy@users.noreply.github.com>
2021-03-22 15:51:52 +0200

    got it working

--- spam.py
+++ spam.py
@@ -13,25 +13,30 @@ time.sleep(5)
 
 img = PhotoImage(file=r"")
 
-textBox = Text(root, height=3, width=29, font=("Helvetica", 14)).place(x=38, y=200)
-
 
 def startSpam():
-    INPUT = textBox.get("1.0", "end-1c")
-    for word in INPUT:
-        time.sleep(0.3)
-        pyautogui.typewrite(word)
-        pyautogui.press("enter")
+    input = Enter.get()
+    while True:
+        for word in input:
+            time.sleep(0.3)
+            pyautogui.typewrite(word)
+            pyautogui.press("enter")
+
+        if pyautogui.press("enter") == 9:
+            time.sleep(2)
+        else:
+            time.sleep(0.3)
 
 
 def stopSpam():
     quit()
 
 
-btn = Button(root, text = 'Start').place(x=100, y=350)
-btn['command'] = startSpam()
+Enter = Entry(root, width=53)
+Enter.place(x=38, y=200)
 
-btn2 = Button(root, text = 'Stop').place(x=230, y=350)
-btn2['command'] = stopSpam()
+btn = Button(root, text='Start', command=lambda: startSpam()).place(x=100, y=350)
+btn2 = Button(root, text='Stop', command=lambda: stopSpam()).place(x=230, y=350)
+btn3 = Button(root, text='i').place(x=360, y=460)
 
 root.mainloop()
